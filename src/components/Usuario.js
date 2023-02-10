f6981311b14f7ed9d4c91d2b21f13cc0ac734099
bvv94 <bvdevasconcelos@gmail.com>
2023-02-10 08:15:23 -0400

    AtualizandoNome

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -3,23 +3,40 @@ import React from "react"
 export default function Usuario() {
 
     const [nome, setNome] = React.useState("catanacomics")
+    const [img, setImg] = React.useState("assets/img/catanacomics.svg")
 
     return (
         <div class="usuario">
-            <img data-test="profile-image" src="assets/img/catanacomics.svg" alt="imagem de perfil" />
+            <img data-test="profile-image" onClick={TrocarImg} src={img} alt="imagem de perfil" />
             <div class="texto">
                 <span>
                     <strong data-test="name">{nome}</strong>
-                    <ion-icon data-test="edit-name" onClick={Trocar} name="pencil"></ion-icon>
+                    <ion-icon data-test="edit-name" onClick={TrocarUsuario} name="pencil"></ion-icon>
                 </span>
             </div>
         </div>
     )
 
-    function Trocar() {
+    function TrocarUsuario() {
         let novonome = prompt("Qual seu nome?")
+        if ((novonome!==null)&&(novonome!==undefined)&&(novonome!=="")) {
+            setNome(novonome)
+        }
+        else{
+            alert("Nome Inválido. Tente novamente!")
+        }
+    }
+
+    function TrocarImg() {
+        let novaimg = prompt("Informe a URL: ")
         
-        setNome(novonome)
+        if ((novaimg!==null)&&(novaimg!==undefined)&&(novaimg!=="")){
+            setImg(novaimg)
+        }
+
+        else{
+            alert("Imagem Inválida. Tente novamente!")
+        }
     }
 }
 
