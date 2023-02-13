0400004d5451d96eecd635b220f2799cc0ec56dd
bvv94 <bvdevasconcelos@gmail.com>
2023-02-13 08:10:26 -0400

    Curtindo com DoubleClick

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,6 +5,7 @@ export default function Post(props) {
     const [sal, setSal] = useState("bookmark-outline")
     const [curt, setCurt] = useState("heart-outline")
     const [cor, setCor] = useState("")
+    const [curtido, setCurtido] = useState(false)
     let [nCurt, setnCurt] = useState(11456)
 
 
@@ -22,18 +23,18 @@ export default function Post(props) {
             </div>
 
             <div class="conteudo">
-                <img data-test="post-image" src={props.imgp} alt="gato-telefone" />
+                <img onDoubleClick={() => Addcurt()} data-test="post-image" src={props.imgp} alt="gato-telefone" />
             </div>
 
             <div class="fundo">
                 <div class="acoes">
                     <div>
-                        <ion-icon class={cor} data-test="like-post" onClick={() => Curtir()} name= {curt}></ion-icon>
+                        <ion-icon class={cor} data-test="like-post" onClick={() => Curtir()} name={curt}></ion-icon>
                         <ion-icon name="chatbubble-outline"></ion-icon>
                         <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
                     <div>
-                        <ion-icon data-test="save-post" onClick={()=> Salvar()} name={sal}></ion-icon>
+                        <ion-icon data-test="save-post" onClick={() => Salvar()} name={sal}></ion-icon>
                     </div>
                 </div>
 
@@ -47,31 +48,41 @@ export default function Post(props) {
         </div>
     )
 
-function Curtir(curtidas){
-   
-    console.log("entrou curtir")
-    console.log(curt)
+    function Curtir(curtidas) {
 
-    if(curt === "heart-outline"){
-        setCurt("heart")
-        setCor("vermelho")
-        setnCurt (nCurt + 1)
-    }
-    else {
-        setCurt("heart-outline")
-        setCor("")
-        setnCurt (nCurt - 1)
-    }
-}
+        console.log("entrou curtir")
+        console.log(curt)
 
-function Salvar(){
-    if(sal === "bookmark-outline"){
-        setSal("bookmark")
+        if ((curt === "heart-outline") && (curtido == false)) {
+            setCurt("heart")
+            setCor("vermelho")
+            setnCurt(nCurt + 1)
+            setCurtido(true)
+        }
+        else {
+            setCurt("heart-outline")
+            setCor("")
+            setnCurt(nCurt - 1)
+            setCurtido(false)
+        }
     }
-    else {
-        setSal("bookmark-outline")
+
+    function Salvar() {
+        if (sal === "bookmark-outline") {
+            setSal("bookmark")
+        }
+        else {
+            setSal("bookmark-outline")
+        }
     }
-}
+    function Addcurt() {
+        if (!curtido) {
+            setCurt("heart")
+            setCor("vermelho")
+            setnCurt(nCurt + 1)
+            setCurtido(!curtido)
+        }
+    }
 
 
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -22,14 +22,24 @@ export default function Posts() {
             curtidas: 157935,
             salvo: true,
             curtido: true
+        },
+        {
+            nomeperfil: "meowed",
+            imgperfil: "./assets/img/meowed.svg",
+            imgpost: "./assets/img/gato-telefone.svg",
+            imgcurtida: "assets/img/respondeai.svg",
+            nomecurtida: "respondeai",
+            curtidas: 101945,
+            salvo: true,
+            curtido: true
         }
     ]
 
     return (
         <div className="posts">
-            {posts.map((p)=> <Post key={p.nomeperfil} nome={p.nomeperfil} imgpf={p.imgperfil} 
-                                imgp={p.imgpost} imgcurtida={p.imgcurtida} curtidas={p.curtidas}
-                                nomecurtida={p.nomecurtida} curtido={p.curtido} salvo={p.salvo}/>)}
+            {posts.map((p) => <Post key={p.nomeperfil} nome={p.nomeperfil} imgpf={p.imgperfil}
+                imgp={p.imgpost} imgcurtida={p.imgcurtida} curtidas={p.curtidas}
+                nomecurtida={p.nomecurtida} curtido={p.curtido} salvo={p.salvo} />)}
         </div>
     )
 }
